d88209af0d21ad68ba76c2c6cc88379babf30041
Joanna Ramasawmy <joanna.ramasawmy@stfc.ac.uk>
2021-05-27 12:39:13 +0100

    pcost.py added

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+
+cost = 0
+
+with open("Data/portfolio.csv", "rt") as f:
+    headers = next(f)
+    print(headers)
+    for line in f:
+        row = line.split(',')
+        num_shares = int(row[1])
+        share_price = float(row[2])
+        tot_price = num_shares*share_price
+        cost += tot_price
+
+print(f'Total price: ${cost}')
