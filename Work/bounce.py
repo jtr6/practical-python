0679f53b2991f72d79be07a50f107a5b3136b3f8
Joanna Ramasawmy <joanna.ramasawmy@stfc.ac.uk>
2021-05-27 10:07:05 +0100

    exercises up to 1.8, day 1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height_init = 100       # height in meters
+height = height_init
+bounce = 0              # number of bounces
+
+while bounce < 10:
+    bounce += 1
+    height = 0.6 * height
+    print(bounce, round(height, 4))
+
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,27 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+base_payment = 2684.11
+total_paid = 0.0
+month = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+
+while principal > 0:
+    payment = base_payment
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
+        payment += extra_payment
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment    
+    month += 1
+    print(month, round(total_paid,4) , round(principal, 4))
+
+if principal < 0:
+    total_paid = total_paid + principal
+
+print('Total paid', round(total_paid, 4), " in {} months".format(month))
