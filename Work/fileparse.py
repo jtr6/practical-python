5ea6c4ef8bde00785c551eefadb4aca75699e5b7
Joanna Ramasawmy <joanna.ramasawmy@stfc.ac.uk>
2021-06-16 12:29:23 +0100

    exercise up to 3.10

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,10 +5,12 @@
 
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
     '''
     Parse a CSV file into a list of records
     '''
+    if select and not has_headers:
+        raise RuntimeError("select argument requires a file with column headers")
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
         # Read the file headers
@@ -24,13 +26,21 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
         else:
             indices = []
         records = []
-        for row in rows:
+        for i, row in enumerate(rows):
             if not row:   # Skip rows with no data
                 continue
             if indices:
                 row = [row[index] for index in indices]
             if types:   # if types are given, convert values to types
-                row = [func(val) for func, val in zip(types, row)]
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except ValueError as e:
+                    if silence_errors:
+                        continue
+                    else:
+                        print(f"Row {i+1}: couldn't parse {row}")
+                        print("Reason:", e)
+                        continue
             if has_headers:
                 record = dict(zip(headers,row))
             else:
