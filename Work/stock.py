5b599c8302f427fc39efafc7d6bda1e0a9f74041
Joanna Ramasawmy <joanna.ramasawmy@stfc.ac.uk>
2021-07-12 17:17:40 +0100

    added stock.py to define Stock class

---
+++ Work/stock.py
@@ -0,0 +1,14 @@
+# File to create class "Stock"
+
+class Stock:
+     
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, sales):
+        self.shares -= sales
